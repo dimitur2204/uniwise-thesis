2d2cb6e12b910cc2f7f81db7568a236ece697c96
dimitar.nizamov <dimitur2204@gmail.com>
2023-05-09 13:41:33 +0300

    dimitar - add example components to the entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import './App.css'
-import { Button, Chip, Tab, Tabs, ThemeProvider } from '@mui/material'
+import { Button, Chip, MenuItem, Select, Tab, Tabs, TextField, ThemeProvider } from '@mui/material'
 import { theme } from './theme'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-    <Button variant="contained" color="primary">Hello World</Button>
-    <Tabs value={1}  aria-label="basic tabs example">
-    <Tab label="Item One"  />
-    <Tab label="Item Two" />
-    <Tab label="Item Three"/>
-  </Tabs>
-  <Chip label="Basic Chip" />
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      width: 200
+    }} className="App">
+      <Button variant="contained" color="primary">Hello World</Button>
+      <Tabs value={1}  aria-label="basic tabs example">
+      <Tab label="Item One"  />
+      <Tab label="Item Two" />
+      <Tab label="Item Three"/>
+      </Tabs>
+      <Chip label="Basic Chip" />
+      <TextField
+            id="standard-helperText"
+            label="Helper text"
+            defaultValue="Default Value"
+            helperText="Some important text"
+            variant="standard"
+          />
+                  <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          label="Age"
+          variant='standard'
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
     </div>
     </ThemeProvider>
 
