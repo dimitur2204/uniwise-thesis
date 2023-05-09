ee776e01dfcc8cb2141a8eb58cd5a075fc91a155
dimitar.nizamov <dimitur2204@gmail.com>
2023-05-09 13:18:17 +0300

    dimitar - add mui dependencies and font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import '@fontsource/signika/300.css';
+import '@fontsource/signika/400.css';
+import '@fontsource/signika/500.css';
+import '@fontsource/signika/700.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
