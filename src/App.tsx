df582e73c210461736761a48a9c0eb245b745c00
dimitar.nizamov <dimitur2204@gmail.com>
2023-05-09 14:01:08 +0300

    dimitar - add flow-ui-header to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import './App.css'
 import { Button, Chip, MenuItem, Select, Tab, Tabs, TextField, ThemeProvider } from '@mui/material'
 import { theme } from './theme'
+import {MainHeader, SelectedRole, WfPageTitleRoot} from '@uniwise/flow-ui-react'
+import { get } from '@uniwise/jwt';
+
 
 function App() {
+  const labelBundle = {
+    admin: "Admin",
+    adminLoginWarning: "You are logged as admin",
+    assessor: "Assessor",
+    author: "Author",
+    create: "Create",
+    editProfile: "Edit profile",
+    invigilator: "Invigilator",
+    logOff: "Log off",
+    logOffAdmin: "Log off admin",
+    manager: "Manager",
+    newFlow:  "New flow",
+    newFlowCombined:  "New flow",
+    newFlowSeries: "New flow series",
+    participant: "Participant",
+    reviewer: "Reviewer",
+    support: "Support",
+    supporter: "Supporter",
+    supportLinkText: "Contact support",
+    wiseflowLogoLinkText: "WiseFlow",
+  };
   return (
     <ThemeProvider theme={theme}>
+            <WfPageTitleRoot licenseName={''} />
+
+      <MainHeader labelBundle={labelBundle}
+      JWT={get()}
+      selectedRole={SelectedRole.ADMIN}/>
     <div style={{
       display: "flex",
       flexDirection: "column",
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '@fontsource/signika/400.css';
 import '@fontsource/signika/500.css';
 import '@fontsource/signika/700.css';
 import './index.css'
+import './assets/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
