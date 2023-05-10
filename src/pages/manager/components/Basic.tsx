a8b44d1cd8813ed61954314c3742ea44a7b629cf
dimitar.nizamov <dimitur2204@gmail.com>
2023-05-10 17:22:59 +0300

    dimitar - finish basic info card

--- src/pages/manager/components/Basic.tsx
+++ src/pages/manager/components/Basic.tsx
@@ -1,9 +1,21 @@
-import { Card, Unstable_Grid2 as Grid2, Select, Stack, TextField, Typography } from '@mui/material';
+import {
+  Card,
+  Unstable_Grid2 as Grid2,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { ActionCard } from './ActionCard';
 import flowLockIcon from '../../../assets/flow-type.svg';
 import { CARD_BORDER } from '../../../theme';
+import { useState } from 'react';
 
 export const Basic = () => {
+  const [exType, setExType] = useState('');
+  const [purpose, setPurpose] = useState('');
+  const [enabled, setEnabled] = useState(false);
   return (
     <Grid2 container gap={3} padding={3} component={Card} variant="outlined">
       <Grid2 xs={12} md={6}>
@@ -12,6 +24,8 @@ export const Basic = () => {
           contentSx={{
             padding: 0,
           }}
+          onClickEdit={() => setEnabled(true)}
+          onClickSave={() => setEnabled(false)}
         >
           <Stack borderBottom={`1px solid ${CARD_BORDER}`} padding={2} direction="row" gap={3}>
             <Stack alignItems="center" justifyContent="center">
@@ -34,16 +48,42 @@ export const Basic = () => {
           </Stack>
           <Grid2 container spacing={5} padding={2}>
             <Grid2 xs={12} md={6}>
-              <TextField fullWidth variant="standard" label="Subtitle" />
+              <TextField disabled={!enabled} fullWidth variant="standard" label="Subtitle" />
             </Grid2>
             <Grid2 xs={12} md={6}>
-              <TextField fullWidth variant="standard" label="Term" />
+              <TextField disabled={!enabled} fullWidth variant="standard" label="Term" />
             </Grid2>
             <Grid2 xs={12} md={6}>
-              <Select fullWidth variant="outlined" size="small" />
+              <TextField
+                disabled={!enabled}
+                select
+                value={exType}
+                onChange={(e) => {
+                  setExType(e.target.value as string);
+                }}
+                fullWidth
+                variant="outlined"
+                size="small"
+                label="Examination type"
+              >
+                <MenuItem value="ord-ex">Ordinary examination</MenuItem>
+              </TextField>
             </Grid2>
             <Grid2 xs={12} md={6}>
-              <TextField fullWidth variant="standard" label="Term" />
+              <TextField
+                disabled={!enabled}
+                select
+                value={purpose}
+                onChange={(e) => {
+                  setPurpose(e.target.value as string);
+                }}
+                fullWidth
+                variant="outlined"
+                size="small"
+                label="Purpose"
+              >
+                <MenuItem value="ex">Examination</MenuItem>
+              </TextField>
             </Grid2>
           </Grid2>
         </ActionCard>
