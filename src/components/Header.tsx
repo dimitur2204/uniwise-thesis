d1debfdbfb70672f47d5c9bc0433931aa8e5b863
dimitar.nizamov <dimitur2204@gmail.com>
2023-05-11 11:35:31 +0300

    dimitar - add button group and language select

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
-import { Box, Tab, Tabs, styled } from '@mui/material';
+import {
+  Box,
+  Tab,
+  Tabs,
+  styled,
+  TextField,
+  Autocomplete,
+  Stack,
+  ButtonGroup,
+  Button,
+} from '@mui/material';
 import logoWhite from '../assets/logo-white.svg';
 import { common, grey } from '@mui/material/colors';
 import { Container } from './Container';
 import { useState } from 'react';
-
+import { LanguageSelect } from './LanguageSelect';
+import { Create, HdrPlus, Menu, PlusOne } from '@mui/icons-material';
 const HeaderTab = styled(Tab)(() => ({
   textTransform: 'none',
   color: common.white,
@@ -14,11 +25,13 @@ const HeaderTab = styled(Tab)(() => ({
     borderTopRightRadius: 5,
   },
 }));
+
 const HeaderTabs = styled(Tabs)(() => ({
   '& .MuiTabs-indicator': {
     backgroundColor: common.white,
   },
 }));
+
 export const Header = () => {
   const [activeTab, setActiveTab] = useState(0);
   return (
@@ -28,22 +41,35 @@ export const Header = () => {
       }}
     >
       <Container>
-        <img src={logoWhite} alt="logo" />
-        <HeaderTabs
-          sx={{
-            color: common.white,
-          }}
-          onChange={(e, value) => setActiveTab(value)}
-          value={activeTab}
-        >
-          <HeaderTab label="Participation" />
-          <HeaderTab label="Invigilation" />
-          <HeaderTab label="Authoring" />
-          <HeaderTab label="Reviewing" />
-          <HeaderTab label="Managing" />
-          <HeaderTab label="Support" />
-          <HeaderTab label="License administration" />
-        </HeaderTabs>
+        <Stack direction="row" justifyContent="space-between">
+          <img src={logoWhite} alt="logo" />
+          <LanguageSelect />
+        </Stack>
+        <Stack direction="row" justifyContent="space-between">
+          <HeaderTabs
+            sx={{
+              color: common.white,
+            }}
+            onChange={(e, value) => setActiveTab(value)}
+            value={activeTab}
+          >
+            <HeaderTab label="Participation" />
+            <HeaderTab label="Invigilation" />
+            <HeaderTab label="Authoring" />
+            <HeaderTab label="Reviewing" />
+            <HeaderTab label="Managing" />
+            <HeaderTab label="Support" />
+            <HeaderTab label="License administration" />
+          </HeaderTabs>
+          <ButtonGroup size="small" variant="contained" color="secondary">
+            <Button size="small" startIcon={<Create />}>
+              Create
+            </Button>
+            <Button size="small" startIcon={<Menu />}>
+              Combine
+            </Button>
+          </ButtonGroup>
+        </Stack>
       </Container>
     </Box>
   );
--- src/pages/manager/index.tsx
+++ src/pages/manager/index.tsx
@@ -2,7 +2,6 @@ import { Button, Card, Chip, MenuItem, Select, Tab, Tabs, TextField } from '@mui
 
 export const Manager = () => {
   return (
-<<<<<<< Updated upstream
     <div
       style={{
         display: 'flex',
@@ -12,10 +11,6 @@ export const Manager = () => {
       className="App"
     >
       <Button variant="contained" color="primary">
-=======
-    <div>
-      {/* <Button variant="contained" color="primary">
->>>>>>> Stashed changes
         Hello World
       </Button>
       <Tabs value={1} aria-label="basic tabs example">
@@ -43,56 +38,7 @@ export const Manager = () => {
         <MenuItem value={10}>Ten</MenuItem>
         <MenuItem value={20}>Twenty</MenuItem>
         <MenuItem value={30}>Thirty</MenuItem>
-<<<<<<< Updated upstream
       </Select>
-=======
-      </Select> */}
-      <Title
-        title="Flow Title"
-        description="Flow subtitle and what the flow is about here"
-        icon="https://via.placeholder.com/60"
-      />
-      <Card variant="outlined" sx={{ mt: 2 }}>
-        <Tabs
-          value={activeTab}
-          onChange={(e, v) => {
-            setActiveTab(v);
-          }}
-          aria-label="basic tabs example"
-          sx={{
-            backgroundColor: 'background.paper',
-            borderBottomWidth: 1,
-          }}
-        >
-          <Tab label="Basic" {...a11yProps(ActiveTab.BASIC)} />
-          <Tab label="Hand out" {...a11yProps(ActiveTab.HAND_OUT)} />
-          <Tab label="Associates" {...a11yProps(ActiveTab.ASSOCIATES)} />
-          <Tab label="Rooms" {...a11yProps(ActiveTab.ROOMS)} />
-          <Tab label="Sources" {...a11yProps(ActiveTab.SOURCES)} />
-          <Tab label="Combine" {...a11yProps(ActiveTab.COMBINE)} />
-          <Tab label="Advanced" {...a11yProps(ActiveTab.ADVANCED)} />
-        </Tabs>
-      </Card>
-      <TabPanel value={ActiveTab.BASIC} activeTab={activeTab}>
-        <Basic />
-      </TabPanel>
-      <TabPanel value={ActiveTab.HAND_OUT} activeTab={activeTab}></TabPanel>
-      <TabPanel value={ActiveTab.ASSOCIATES} activeTab={activeTab}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={ActiveTab.ROOMS} activeTab={activeTab}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={ActiveTab.SOURCES} activeTab={activeTab}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={ActiveTab.COMBINE} activeTab={activeTab}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={ActiveTab.ADVANCED} activeTab={activeTab}>
-        Item Seven
-      </TabPanel>
->>>>>>> Stashed changes
     </div>
   );
 };
